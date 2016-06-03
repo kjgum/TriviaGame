4ae998faaf418a281d522f9cdbf8f473da32ff53
Kristin Gumbert <kristingumbert@Kristins-MacBook-Pro.local>
2016-06-03 01:11:32 -0400

    finalized questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,67 +5,67 @@ var game = {
       question: 'Which of these terms means to cook food in its own juices with a small amount of fat over low heat, just until softened?',
       answer: 'sweating',
       wrongAnswerAry: ['simmering','stewing','sauteeing'],
-      img: '../../1.jpg'
+      img: '../1.jpg'
     },
     1: {
-      question: "What's the difference between a convection oven and a conventional oven?",
-      answer: 'A convection oven uses a fan.',
-      wrongAnswerAry: ['A convection oven uses radioactivity.','A convection oven lacks the broiler setting.','convention oven only goes up to 300 degrees.'],
-      img: '../../1.jpg'
+      question: "In molecular gastronomy, what is the primary purpose of using liquid nitrogen?",
+      answer: 'freezing',
+      wrongAnswerAry: ['heating','blanching','caramelizing'],
+      img: '../1.jpg'
     },
     2: {
       question: 'Which of these classic cuts is the smallest?',
       answer: 'brunoise',
       wrongAnswerAry: ['julienne','cube','dice'],
-      img: '../../1.jpg'
+      img: '../1.jpg'
     },
     3: {
       question: 'To keep foods moist during cooking by brushing, drizzling or spooning on a sauce, pan juices, or wine. What is the term for this?',
       answer: 'baste',
       wrongAnswerAry: ['dredge','soak','glaze'],
-      img: '../../1.jpg'
+      img: '../1.jpg'
     },
     4: {
       question: 'To decorate food with fresh herbs, edible flowers, fresh vegetables, or fruit to enhance the look and taste of the dish. What is the term for this?',
       answer: 'garnish',
       wrongAnswerAry: ['refresh','plump','fix'],
-      img: '../../1.jpg'
+      img: '../1.jpg'
     },
     5: {
-      question: "To add an ingredient such as eggs, which hold together the other ingredients, as in meatloaf. What is the term for this?",
-      answer: 'bind',
-      wrongAnswerAry: ['blend', 'curdle', 'combine'],
-      img: '../../1.jpg'
+      question: "What part of a cow does filet mignon come from?",
+      answer: 'loin',
+      wrongAnswerAry: ['belly', 'leg', 'neck'],
+      img: '../1.jpg'
     },
     6: {
       question: "This term is normally used in baking. It means to combine butter, sugar, egg and flavoring until light and fluffy.",
       answer: 'fold',
       wrongAnswerAry: ['mix','whip','cream'],
-      img: '../../1.jpg'
+      img: '../1.jpg'
     },
     7: {
-      question: 'question',
-      answer: '6',
-      wrongAnswerAry: ['1','1','1'],
-      img: '../../1.jpg'
+      question: 'If you were julienning an onion, the resulting slices would be about the size of what?',
+      answer: 'matchstick',
+      wrongAnswerAry: ['fingernail','straight pin','english pea'],
+      img: '../1.jpg'
     },
     8: {
-      question: 'question',
-      answer: '7',
-      wrongAnswerAry: ['1','1','1'],
-      img: '../../1.jpg'
+      question: 'What is the technique for removing the browned bits from a pan to flavor sauces, soups and gravies?',
+      answer: 'deglazing',
+      wrongAnswerAry: ['making a roux','sauteeing','caramelizing'],
+      img: '../1.jpg'
     },
     9: {
-      question: 'question',
-      answer: '8',
-      wrongAnswerAry: ['1','1','1'],
-      img: '../../1.jpg'
+      question: 'The soaking of a fruit in a flavored liquid so it penetrates the fruit is called …',
+      answer: 'macerate',
+      wrongAnswerAry: ['reconstitute','smoothie','puree'],
+      img: '../1.jpg'
     },
     10: {
-      question: 'question',
-      answer: '9',
-      wrongAnswerAry: ['1','1','1'],
-      img: '../../1.jpg'
+      question: 'What is the preservation method called that cooks food and preserves it in its own fat?',
+      answer: 'confit',
+      wrongAnswerAry: ['salt cure','gelatin','braise'],
+      img: '../1.jpg'
     }
   },
   // in-game variables
@@ -113,8 +113,7 @@ var game = {
         usedNum.push(randomNum);
       }
     } while (randomizedAry.length < ary.length)
-    console.log(`randomized---> ${randomizedAry}`);
-    console.log(`usedNum---> ${usedNum}`);
+   
     return randomizedAry;
   },
   // go to next question
@@ -122,10 +121,10 @@ var game = {
     self = this;
     // update timer
     self.timeLeft = self.quesTime;
-    $('.info').html($('<span>').text('Time Left: '+self.timeLeft));
+    $('.info').html($('<span>').text('time left: '+self.timeLeft));
     self.timer = setInterval(function() {
       self.timeLeft--;
-      $('.info').html($('<span>').text('Time Left: '+self.timeLeft));
+      $('.info').html($('<span>').text('time left: '+self.timeLeft));
       // if out of time clear timer and move to next question
       if (self.timeLeft === 0) {
         console.log("time is up!");
@@ -161,9 +160,11 @@ var game = {
       console.log('wrongAnswers:',self.wrongAnswers);
     }
     // display answer
-    let info = isCorrect ? 'Correct!' : 'the correct answer was: '+self.currentAnswer
+    let info = isCorrect ? 'CORRECT!' : 'the correct answer was: '+self.currentAnswer
     $('div.info').html(info);
+    
     $('div.board').empty();
+
     // next question or game over
     setTimeout(function() { 
       if (self.quesNum+1 > self.$questionNodeAry.length) {
@@ -240,13 +241,13 @@ $( document ).ready(function() {
 });
 
 
-// game started
+// game start
 $(document).on('click', '.start', function() {
   game.nextQuestion();
 });
 
 
-// answer selected
+// answer select
 $(document).on('click', '.btn-custom', function() {
   let thisAnswer = $(this).data('ans');
   game.guessedAnswer(String(thisAnswer));
